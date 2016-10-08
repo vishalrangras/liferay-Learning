87379ad81d1f7e89db6e6932e5515fcdf3b0bb0f
vishalrangras <vishalrangras@yahoo.com>
2016-10-09 00:20:13 +0530

    Update PRProductLocalServiceImpl.java
    Adding deleteProduct() method having 2 overloaded versions

--- Raw-Code/src-code/PRProductLocalServiceImpl.java
+++ Raw-Code/src-code/PRProductLocalServiceImpl.java
@@ -16,4 +16,19 @@ product.setGroupId(newProduct.getGroupId());
 return prProductPersistence.update(product, false);
 }
 
+public void deleteProduct(long productId)
+throws NoSuchProductException, SystemException, PortalException {
+PRProduct product =
+prProductPersistence.findByPrimaryKey(productId);
+deleteProduct(product);
+}
+
+public void deleteProduct(PRProduct product)
+throws PortalException, SystemException {
+resourceLocalService.deleteResource(
+product.getCompanyId(), PRProduct.class.getName(),
+ResourceConstants.SCOPE_INDIVIDUAL, product.getPrimaryKey());
+prProductPersistence.remove(product);
+}
+
 docroot/WEB-INF/src/com/inkwell/internet/productregistration/service/impl/PRProductLocalServiceImpl.java
