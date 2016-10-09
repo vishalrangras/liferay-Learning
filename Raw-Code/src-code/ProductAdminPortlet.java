c02d7c8a69a733a3a689706237d028a5f0efc6bc
vishalrangras <vishalrangras@yahoo.com>
2016-10-09 14:03:29 +0530

    Update ProductAdminPortlet.java
    Added editProduct() method

--- Raw-Code/src-code/ProductAdminPortlet.java
+++ Raw-Code/src-code/ProductAdminPortlet.java
@@ -1,17 +1,29 @@
-public void addProduct(ActionRequest request, ActionResponse response)
-throws Exception {
-ThemeDisplay themeDisplay =
-(ThemeDisplay) request.getAttribute(WebKeys.THEME_DISPLAY);
-PRProduct product = ActionUtil.productFromRequest(request);
-ArrayList<String> errors = new ArrayList<String>();
-if (ProdRegValidator.validateProduct(product, errors)) {
-PRProductLocalServiceUtil.addProduct(
-product,themeDisplay.getUserId());
-SessionMessages.add(request, "product-saved-successfully");
+public void addProduct(ActionRequest request, ActionResponse response) throws Exception {
+  
+  ThemeDisplay themeDisplay = (ThemeDisplay) request.getAttribute(WebKeys.THEME_DISPLAY);
+  PRProduct product = ActionUtil.productFromRequest(request);
+  ArrayList<String> errors = new ArrayList<String>();
+  
+  if (ProdRegValidator.validateProduct(product, errors)) {
+      PRProductLocalServiceUtil.addProduct(product,themeDisplay.getUserId());
+      SessionMessages.add(request, "product-saved-successfully");
+  }
+  else {
+      SessionErrors.add(request, "fields-required");
+  }
 }
-else {
-SessionErrors.add(request, "fields-required");
-}
+
+public void editProduct(ActionRequest request, ActionResponse response) throws Exception {
+
+  long productKey = ParamUtil.getLong(request, "resourcePrimKey");
+
+  if (Validator.isNotNull productKey)) {
+
+    PRProduct product = PRProductLocalServiceUtil.getPRProduct(productKey);
+    
+    request.setAttribute("product", product);
+    response.setRenderParameter("jspPage", editProductJSP);
+  }
 }
 
 //docroot/WEB-INF/src/com/inkwell/internet/productregistration/portlet/ProductAdminPortlet.java
