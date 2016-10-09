a82d3e227ae139ca0955fcf913b79b612ef2d085
vishalrangras <vishalrangras@yahoo.com>
2016-10-09 14:33:33 +0530

    Update ProductAdminPortlet.java
    Added the code to update or delete the product

--- Raw-Code/src-code/ProductAdminPortlet.java
+++ Raw-Code/src-code/ProductAdminPortlet.java
@@ -26,4 +26,66 @@ public void editProduct(ActionRequest request, ActionResponse response) throws E
   }
 }
 
+   /**
+     * This Action updates an existing product with values that were entered
+     * into the edit_product.jsp.
+     *
+     * @param request
+     * @param response
+     * @throws java.lang.Exception
+     */
+
+    public void updateProduct(ActionRequest request, ActionResponse response)
+        throws Exception {
+
+        long productKey = ParamUtil.getLong(request, "resourcePrimKey");
+        ArrayList<String> errors = new ArrayList();
+        if (Validator.isNotNull(productKey)) {
+            PRProduct product =
+                PRProductLocalServiceUtil.getPRProduct(productKey);
+            PRProduct requestProduct = ActionUtil.productFromRequest(request);
+
+            if (ProdRegValidator.validateProduct(requestProduct, errors)) {
+                product.setProductName(requestProduct.getProductName());
+                product.setSerialNumber(requestProduct.getSerialNumber());
+                PRProductLocalServiceUtil.updatePRProduct(product);
+                SessionMessages.add(request, "productUpdated");
+
+            }
+            else {
+                for (String error : errors) {
+                    SessionErrors.add(request, error);
+
+                }
+
+            }
+
+        }
+        else {
+            SessionErrors.add(request, "error-updating");
+        }
+
+    }
+
+    /**
+     * This Action deletes a product from the database.
+     *
+     * @param request
+     * @param response
+     * @throws java.lang.Exception
+     */
+    public void deleteProduct(ActionRequest request, ActionResponse response)
+        throws Exception {
+
+        long productKey = ParamUtil.getLong(request, "resourcePrimKey");
+
+        if (Validator.isNotNull(productKey)) {
+            PRProductLocalServiceUtil.deleteProduct(productKey);
+            SessionMessages.add(request, "productDeleted");
+        }
+        else {
+            SessionErrors.add(request, "error-deleting");
+        }
+    }
+
 //docroot/WEB-INF/src/com/inkwell/internet/productregistration/portlet/ProductAdminPortlet.java
